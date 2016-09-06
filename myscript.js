413f242b82daa20f67cbc45ce15755c6d5b6c0fd
aheilgeist <aheilgeist@students.mchenry.edu>
2016-09-05 19:11:42 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,55 @@
+function initialize() {
+    var text = "";
+
+    document.getElementById("greeting").innerHTML = "Photo of a rustic wood post fence with pretty wildflowers";
+}
+
+function change01(){
+    document.getElementById("large").src="images/fence.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of a rustic wood post fence with pretty wildflowers';
+}
+
+function change02(){
+    document.getElementById("large").src="images/fountain.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of a fountain in my subdivision framed by trees';
+}
+
+function change03(){
+document.getElementById("large").src="images/gazebo.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of ceiling of a gazebo';
+}
+
+function change04(){
+document.getElementById("large").src="images/lexi.jpg";
+    document.getElementById("greeting").innerHTML = 'My fur-baby, Lexi';
+}
+
+function change05(){
+document.getElementById("large").src="images/school.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of hallway in MCC';
+}
+
+function change06(){
+    document.getElementById("large").src="images/sky.jpg";
+    document.getElementById("greeting").innerHTML = 'Cool looking sky';
+}
+
+function change07(){
+    document.getElementById("large").src="images/tombydam.jpg";
+    document.getElementById("greeting").innerHTML = 'My friend, Tom, by the McHenry Dam';
+}
+
+function change08(){
+    document.getElementById("large").src="images/tomsleeping.jpg";
+    document.getElementById("greeting").innerHTML = 'My friend, Tom, being silly - pretending to be sleeping';
+}
+
+function change09(){
+    document.getElementById("large").src="images/treeline.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of a treeline in the MCC parking lot';
+}
+
+function change10(){
+    document.getElementById("large").src="images/yellowflowers.jpg";
+    document.getElementById("greeting").innerHTML = 'Photo of some pretty summer flowers';
+}
